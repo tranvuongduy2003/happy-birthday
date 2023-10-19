8aaff23addc673a85888e56f844d036f7c94bcd0
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2023-10-19 23:49:55 +0700

    new code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Candle } from "./components/candle/Candler";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main">
+      <img src="an1.jpg" alt="" className="image1" />
+      <img src="an2.jpg" alt="" className="image2" />
+      <div className="heading">
+        <h1>Chúc mừng sinh nhật Annh Ngọc 🎉🎉🎉</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="cake">
+        <div className="candle-line">
+          <Candle />
+          <Candle />
+          <Candle />
+        </div>
+        <div className="under"></div>
+        <div className="shadow"></div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/candle/Candler.tsx
@@ -0,0 +1,10 @@
+export interface ICandleProps {}
+
+export function Candle() {
+  return (
+    <div className="candle">
+      <div className="wick"></div>
+      <div className="flame"></div>
+    </div>
+  );
+}
